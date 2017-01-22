01c638e580d0805fab2e6008d946e2150c7a2191
Ben Scott <ias.jazz@gmail.com>
2017-01-22 02:02:07 -0500

    buoyancy and ship motion

--- Assets/Scripts/Moon.cs
+++ Assets/Scripts/Moon.cs
@@ -4,22 +4,18 @@ using UnityEngine;
 
 public class Moon : MonoBehaviour {
     public static Moon singleton {get; private set;}
-    [SerializeField]
-    private float weight;
-    [SerializeField]
-    float movespeed;
+    [SerializeField] protected float weight;
+    [SerializeField] protected float movespeed;
     Vector3 velocity;
-    public float Weight {
-        get{
-            return weight;
-        }
-
-    }
+    public float Weight => weight;
 
     void Awake() => singleton = this;
 
-    void Update(){
-        velocity = new Vector3(Input.GetAxis("Right Horizontal") * movespeed,0f,Input.GetAxis("Right Vertical") * -1f * movespeed); // make this should be adjust to the camera angle
+    void Update() {
+        // make this should be adjust to the camera angle
+        velocity = new Vector3(
+            Input.GetAxis("Right Horizontal") * movespeed,0f,
+            Input.GetAxis("Right Vertical") * -1f * movespeed);
         transform.Translate(velocity * Time.deltaTime);
     }
 
--- Assets/Scripts/Sailboat.cs
+++ Assets/Scripts/Sailboat.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class Sailboat : MonoBehaviour {
     new Rigidbody rigidbody;
     float angularAxis, throttle;
-    [SerializeField] float health = 1000;
-    [SerializeField] float thrust = 1000;
+    [SerializeField] protected float health = 1000;
+    [SerializeField] protected float manuevering = 100;
+    [SerializeField] protected float thrust = 1000;
 
-    void Start() => rigidbody = GetComponent<Rigidbody>();
+    void Awake() => rigidbody = GetComponent<Rigidbody>();
+    void Start() => rigidbody.centerOfMass = Vector3.down*3;
 
-    void Update() => (angularAxis,thrust) =
+    void Update() => (angularAxis, throttle) =
         (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
     void FixedUpdate() {
         rigidbody.AddForce(Vector3.forward*throttle*thrust);
-        rigidbody.AddTorque(Vector3.right*throttle*thrust);
+        rigidbody.AddTorque(Vector3.right*angularAxis*manuevering);
     }
 }
